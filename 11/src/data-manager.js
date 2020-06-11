bf18046a70ebeccdd67519ef97159193d41e70e4
nhwa <44343908+nhwa@users.noreply.github.com>
2020-06-11 17:59:41 +0900

    JSON 서버 이용시

--- 11/src/data-manager.js
+++ 11/src/data-manager.js
@@ -2,10 +2,18 @@ function saveData(data) {
   console.log('todos:', data);
   localStorage.setItem('data', JSON.stringify(data));
 }
-function loadData() {
+function loadData(/* JSON 서버 이용시 callback */) {
+  //local storage 이용
   const dataString = localStorage.getItem('data') || '[]';
-
   return JSON.parse(dataString);
+
+  /*JSON 서버 이용
+    fetch('http://localhost:3000/todos').then((response) => {
+      response.json().then((data) => {
+        callback(data);
+      });
+    });
+    */
 }
 
 export { saveData, loadData };
--- 11/src/index.js
+++ 11/src/index.js
@@ -22,3 +22,23 @@ $result.addEventListener('click', (event) => {
 
 formInput.init(todos);
 render(todos);
+
+/* JSON 서버 이용시
+loadData((todos) => {
+  $result.addEventListener('click', (event) => {
+    const { className } = event.target;
+    if (className === 'delete') {
+      const { index } = event.target.parentElement.dataset;
+      todos.splice(index, 1);
+      render(todos);
+    } else if (className === '') {
+      // const { index } = event.target.parentElement.dataset;
+      // todos[index].isDone = !todos[index].isDone;
+      // render(todos);
+    }
+  });
+
+  formInput.init(todos);
+  render(todos);
+});
+*/
